af5a794f18dd5a8a79a4fc0e71b45294604f4e58
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-03 13:34:18 -0400

    Add project files.

---
+++ ArrayTutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿// Arrays Tutorial
+
+int[] nbrs = { 7, 11, 42, 99, 32, -14, 1, 72, -54, -45 };
+
+var sum = 0;
+var index = 0;
+
+while(index < 10) 
+{
+    if(nbrs[index] % 2 == 0) {
+        Console.WriteLine($"{nbrs[index]} is even.");
+    }
+    sum = sum + nbrs[index];
+    index = index + 1;
+}
+
+Console.WriteLine($"The average is {sum/10}"); // answer 15
+
+
+
+
